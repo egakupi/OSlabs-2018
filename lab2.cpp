f4ea23f61a6022151bc53f796e78cc21622e42e4
Egakupi <dmisavchenkv@gmail.com>
2018-03-16 12:00:14 +0300

    Lab2 completed with trylock and struct of arguments

--- lab2.cpp
+++ lab2.cpp
@@ -5,19 +5,25 @@
 
 using namespace std;
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+struct argument {
+    pthread_mutex_t mutex;
+    bool flag;
+};
 
 void* funcThread1(void* args) {
     cout << "\nThe First thread started. . .\n";
 
-    while (*(bool*)args) {
-        pthread_mutex_lock(&mutex);
-        for(int i = 0; i < 10; i++) {
-            cout << '1' << flush;
-            sleep(1);
+    argument *arg = (argument *) args;
+
+    while (arg->flag) {
+        if (!pthread_mutex_trylock(&arg->mutex)) {
+            for(int i = 0; i < 10; i++) {
+                cout << '1' << flush;
+                usleep(1);
+            }
+            pthread_mutex_unlock(&arg->mutex);
         }
-        pthread_mutex_unlock(&mutex);
-        sleep(1);
+        usleep(1);
     }
 
     pthread_exit((void*)150);
@@ -26,14 +32,17 @@ void* funcThread1(void* args) {
 void* funcThread2(void* args) {
     cout << "The Second thread started. . .\n";
 
-    while (*(bool*)args) {
-        pthread_mutex_lock(&mutex);
-        for(int i = 0; i < 10; i++) {
-            cout << '2' << flush;
-            sleep(1);
+    argument *arg = (argument *) args;
+
+    while (arg->flag) {
+        if (!pthread_mutex_trylock(&arg->mutex)) {
+            for(int i = 0; i < 10; i++) {
+                cout << '2' << flush;
+                usleep(1);
+            }
+            pthread_mutex_unlock(&arg->mutex);
         }
-        pthread_mutex_unlock(&mutex);
-        sleep(1);
+        usleep(1);
     }
 
     pthread_exit((void*)10);
@@ -42,22 +51,26 @@ void* funcThread2(void* args) {
 int main() {
     pthread_t thr1;
     int statusThr1 = 1;
-    bool flag1 = true;
 
     pthread_t thr2;
     int statusThr2 = 1;
-    bool flag2 = true;
 
-    pthread_create(&thr1, NULL, funcThread1, &flag1);
-    pthread_create(&thr2, NULL, funcThread2, &flag2);
+    argument arg;
+    arg.flag = true;
+
+    pthread_mutex_init(&arg.mutex, NULL);
+
+    pthread_create(&thr1, NULL, funcThread1, (void *) &arg);
+    pthread_create(&thr2, NULL, funcThread2, (void *) &arg);
 
     cout << "Press any key (ENTERR) to stop it\n\n";
-
     getchar();
-    flag1 = false;
-    flag2 = false;
-    pthread_join(thr1, (void**)&statusThr1);
-    pthread_join(thr2, (void**)&statusThr2);
+
+    arg.flag = false;
+    pthread_join(thr1, (void **) &statusThr1);
+    pthread_join(thr2, (void **) &statusThr2);
+    pthread_mutex_destroy(&arg.mutex);
+
     cout << "\nThe First thread ended with " << statusThr1 << " code";
     cout << "\nThe Second thread ended with " << statusThr2 << " code";
     cout << "\nEnd of this shit\n";
