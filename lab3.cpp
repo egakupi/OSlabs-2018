faa1e6f1b97d93a4fef3456e79f00d9bf33faaaf
Egakupi <dmisavchenkv@gmail.com>
2018-03-23 11:16:41 +0300

    Lab3 from 16.03

---
+++ lab3.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <unistd.h>
+#include <pthread.h>
+#include <cstdio>
+#include <string.h>
+#include <stdlib.h> // for random
+
+using namespace std;
+
+struct argument {
+    pthread_mutex_t mutex;
+    bool flag;
+};
+
+int fd[2];
+
+void* funcThread1(void* args) { //writer
+    cout << "\nThe First thread started. . .\n";
+
+    argument *arg = (argument *) args;
+    char buf = 'A';
+
+    while (arg->flag) {
+        write(fd[1], &buf, 1);
+        cout << "Writer = " << buf << endl;
+        if (buf == 'Z') {
+            buf = 'A' - 1;
+        }
+        buf++;
+        usleep(100);
+    }
+
+    pthread_exit((void*)150);
+}
+
+void* funcThread2(void* args) { //reader
+    cout << "The Second thread started. . .\n";
+
+    argument *arg = (argument *) args;
+    char buf;
+
+    while (arg->flag) {
+        memset(&buf, '\0', sizeof(char));
+        read(fd[0], &buf, 1);
+        cout << "Reader = " << buf << endl;    
+        usleep(100);
+    }
+
+    pthread_exit((void*)10);
+}
+
+int main() {
+    pthread_t thr1;
+    int statusThr1 = 1;
+
+    pthread_t thr2;
+    int statusThr2 = 1;
+
+    argument arg;
+    arg.flag = true;
+
+    pipe(fd);
+
+    pthread_mutex_init(&arg.mutex, NULL);
+
+    pthread_create(&thr1, NULL, funcThread1, (void *) &arg);
+    pthread_create(&thr2, NULL, funcThread2, (void *) &arg);
+
+    cout << "Press any key (ENTERR) to stop it\n\n";
+    getchar();
+
+    arg.flag = false;
+    pthread_join(thr1, (void **) &statusThr1);
+    pthread_join(thr2, (void **) &statusThr2);
+    pthread_mutex_destroy(&arg.mutex);
+
+    cout << "\nThe First thread ended with " << statusThr1 << " code";
+    cout << "\nThe Second thread ended with " << statusThr2 << " code";
+    cout << "\nEnd of this shit\n";
+    return 0;
+}
